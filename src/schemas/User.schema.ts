6675f6fd97c20d39c518001059390650bc8e48d6
Ghazi <ghazibouzid63@gmail.com>
2024-03-06 01:11:44 +0100

    Crud Lettre

--- src/schemas/User.schema.ts
+++ src/schemas/User.schema.ts
@@ -4,11 +4,8 @@ import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Room } from "./Room.schema";
 
 
-
-
 @Schema()
 export class User {
-
     @Prop({ unique: true, required: true })
     username: string;
   
