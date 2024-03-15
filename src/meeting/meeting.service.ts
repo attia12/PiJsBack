3c47d500f36bc1d39b9659d9341a008e7a862b0f
Ghazi <ghazibouzid63@gmail.com>
2024-03-15 01:30:58 +0100

    Mise a jour

--- src/meeting/meeting.service.ts
+++ src/meeting/meeting.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { LettreDeMotivation } from 'src/schemas/LettreDeMotivation.schema';
+import { Meeting } from 'src/schemas/Meeting.schema';
+import { AjouterMeetingDto } from './dto/ajouterMeetingdto';
 
 @Injectable()
-export class MeetingService {}
+export class MeetingService {
+    constructor(@InjectModel(Meeting.name) private readonly meetingModel:Model<Meeting> ){}
+    async addMeeting(ajouterDto:AjouterMeetingDto,id:string) : Promise<Meeting> {
+       const meeting = new this.meetingModel({
+        lienMeet:ajouterDto.lienMeet,
+        dateDebut:ajouterDto.dateDebut,
+        time:ajouterDto.time,
+        user:id
+       });
+       return meeting.save();
+    }
+ 
+    async findAllByUser(userId: string): Promise<Meeting[]> {
+        return this.meetingModel.find({ user: userId }).exec();
+      }
+}
