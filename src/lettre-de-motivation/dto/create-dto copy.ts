979ade1668e6e3daa33eeea231a99362affea845
Ghazi <ghazibouzid63@gmail.com>
2024-03-15 01:30:58 +0100

    Mise a jour

--- src/lettre-de-motivation/dto/create-dto copy.ts
+++
@@ -1,9 +0,0 @@
-import { FileInterceptor } from '@nestjs/platform-express/multer';
-import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
-
-export class LettreDeMotivationDto {
-  @IsNotEmpty()
-  @MinLength(50)
-  description: string;
-  cv: Express.Multer.File;
-}
