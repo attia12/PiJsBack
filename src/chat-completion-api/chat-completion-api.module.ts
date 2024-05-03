6627ef5f3ec0c1cbdfe74cb31f8838de323f8c26
Ghazi <ghazibouzid63@gmail.com>
2024-05-03 09:58:56 +0100

    adding config

--- src/chat-completion-api/chat-completion-api.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ChatCompletionApiService } from './chat-completion-api.service';
-import { ChatCompletionApiController } from './chat-completion-api.controller';
-
-@Module({
-  providers: [ChatCompletionApiService],
-  controllers: [ChatCompletionApiController]
-})
-export class ChatCompletionApiModule {}
